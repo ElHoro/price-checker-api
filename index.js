3c5f2de3b2b95ec2cc92b4b50a49a504fcbf093c
mikealeman <miguel.aleman@rentcars.com>
2018-03-13 18:43:54 -0300

    Added proclife and setup mlab connection

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 var mongoose = require('mongoose');
 var app = require('./app.js');
 var port = process.env.PORT || 3678;
+var mongodb_uri = process.env.MONGODB_URI;
 
-
-mongoose.connect('mongodb://localhost:27017/price-checker', (err, res) => {
+mongoose.connect(mongodb_uri, (err, res) => {
   if(err){
     throw err;
   }else{
       console.log('Conexión a Mongo correcta');
       app.listen(port, () => {
-        console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
+        console.log(`API REST FAVORITOS funcionando en el puerto :${port}`);
       });
   }
 });
